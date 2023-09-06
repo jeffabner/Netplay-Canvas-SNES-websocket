047a224a1c2efad20d13efda88ac71876f1a7ef8
Abner <33882384+jeffabner@users.noreply.github.com>
2023-09-06 17:09:22 -0300

    Update horde-server.js

--- horde-server.js
+++ horde-server.js
@@ -1,4 +1,4 @@
-const socket = io("ws://localhost:3000/servers");
+const socket = io("wss://servidor-websocket-canvas-snes.onrender.com/servers");
 const clients = new Map();
 
 let room = new URL(window.location.href).searchParams.get('room');
